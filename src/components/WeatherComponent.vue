bf952503a76700710fcb341546b524e14950c79b
Hamza Alahmad <92593709+Hamzi125@users.noreply.github.com>
2025-02-05 10:37:49 +0100

    Update WeatherComponent.vue

--- src/components/WeatherComponent.vue
+++ src/components/WeatherComponent.vue
@@ -1,24 +1,26 @@
 <template>
-    <div>
-        <div
-            class="flex justify-center items-center h-dvh p-5 bg-gray-200 bg-gradient-to-t from-pink-500 to-violet-500 border-10 border-white rounded-4xl">
-            <img src="/src/assets/weather-app.png" alt="logo"
-                class="w-16 h-16 absolute top-9 left-6 animate-spin-slow">
+    <div> 
+        <div :class="{ 'blur-sm': error }"  class="flex justify-center items-center h-dvh p-5 bg-gray-200 bg-gradient-to-t from-pink-500 to-violet-500 border-10 border-white rounded-4xl">
+            <img src="/src/assets/weather-app.png" alt="logo" class="w-16 h-16 absolute top-9 left-6 animate-spin-slow">
             <input type="text"
                 class="w-2/4 p-5 border border-white enabled:hover:border-white rounded-3xl absolute top-9 focus:outline-none text-white"
                 placeholder="Stadt eingeben..." v-model="query" @keypress.enter="fetchWeather">
             <div
                 class="relative bg-transparent max-w-sm w-full h-auto rounded-3xl flex flex-col justify-center items-center shadow-xl p-5 border border-white">
                 <img :src="weather.icon || 'https://openweathermap.org/img/wn/01d@2x.png'" alt="Wetter Icon"
-                    class="w-25 h-25 sm:w-20 sm:h-20 md:w-28 md:h-28 mb-3 ">
-                <h1 class="text-3xl sm:text-2xl md:text-3xl  font-semibold text-amber-50">{{ weather.city || '---' }}
-                </h1>
+                    class="w-25 h-25 sm:w-20 sm:h-20 md:w-28 md:h-28 mb-3">
+                <h1 class="text-3xl sm:text-2xl md:text-3xl  font-semibold text-amber-50">{{ weather.city || '---' }}</h1>
                 <p class="text-xl sm:text-xl md:text-2xl font-light text-amber-50 mt-3">{{ weather.temp !== "" ?
                     weather.temp + "°C" : "0" }}
                 </p>
             </div>
-            <p v-if="error" class="text-white mt-5 flex justify-center absolute bottom-5 left-1/2 transform -translate-x-1/2 ">{{ error }}</p>
         </div>
+        <div v-if="error" class="fixed  top-80 left-0 w-full  bg-white text-center py-2 rounded-full  lg:px-4 z-50">
+                <div class="p-5  leading-none lg:rounded-full flex lg:inline-flex"
+                    role="alert">
+                    <span class="font-semibold mr-2 text-left flex-auto text-violet-400 ">{{ error }}</span>
+                </div>
+            </div>
     </div>
 </template>
 
@@ -63,7 +65,7 @@ const fetchWeather = async () => {
         const response = await axios.get(API_URL_Current);
         const data = response.data;
         if (data.success === false) {
-            error.value = "Fehler: " + data.error.info;
+            error.value = "Error:  " + data.error.info;
             return;
         }
         weather.value = {
